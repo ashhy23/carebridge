130d8ec3f604766772116829c0a31dd5a6878748
Ashitha <ashithag70@gmail.com>
2026-05-22 17:01:05 +0530

    chore: add remaining client config files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Must match server CORS origin (credentials + cookies)
+    port: 5174,
+  },
 })
